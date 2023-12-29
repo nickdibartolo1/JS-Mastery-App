f25872130be584b2ab539822a43d717b8efa661e
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-12-29 18:06:50 -0500

    sign in/up Clerk edits

--- evently/components/ui/shared/Header.tsx
+++ evently/components/ui/shared/Header.tsx
@@ -1,19 +1,27 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../button";
 
 const Header = () => {
   return (
-    <header className='w-full border-b'>
-        <div className='wrapper flex items-center justify-between'></div>
-        <Link href='/' className='w-36'>
-            <Image src="" width={128} height={36} alt="Evently Logo"></Image>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="" width={128} height={36} alt="Evently Logo"></Image>
         </Link>
 
-        <div className='flex w-32 justify-end gap-3'></div>
-
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button className="rounded-full" size="lg" asChild>
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
