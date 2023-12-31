567a29ea3652a6a1c63ae3525aa30ca29df58bec
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-12-31 11:54:04 -0500

    added navLinks, navbar for mobile, sheet + sep from shad

--- evently/components/ui/shared/MobileNav.tsx
+++ evently/components/ui/shared/MobileNav.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "../separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          Open
+          <Image
+            src="/"
+            alt="menu"
+            className="cursor-pointer"
+            width={24}
+            height={24}
+          ></Image>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image src="" alt="logo" width={128} height={38}></Image>
+         <Separator className="border border-gray-50"/>
+         <NavItems></NavItems>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
--- evently/components/ui/shared/NavItems.tsx
+++ evently/components/ui/shared/NavItems.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import React from "react";
 
 const NavItems = () => {
   return (
-    <div>NavItems</div>
-  )
-}
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        return (
+          <li>
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        )
+      })}
+    </ul>
+  );
+};
 
-export default NavItems
+export default NavItems;
