3b4dca210a10b92648482e80d2cf38884b883ce8
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-01 17:55:36 -0500

    db connection setup, alao created db on mongo

--- evently/lib/database/index.ts
+++ evently/lib/database/index.ts
@@ -5,5 +5,17 @@ const MONGODB_URI = process.env.MONGODB_URI
 let cached = (global as any).mongoose || { conn: null, promise: null}; //if cached connection is not already mongoose cached connection then set it to empty object; "(global as any)" = ts trick
 
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn
+    if(cached.conn) return cached.conn; //if connection is good
+
+    if(!MONGODB_URI) throw new Error('MONGO DATABASE is missing URI');
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, { //either connect to existing cached connection OR create new connection
+        dbName: "evently",
+        bufferCommands: false
+    })
+
+    cached.conn = await cached.promise;
+
+    return cached.conn
 }
+
