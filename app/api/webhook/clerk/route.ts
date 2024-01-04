33c14e156dd712c915b5d2bd9b12e57b5d11ff76
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-04 17:55:41 -0500

    fix

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
  
 export async function POST(req: Request) {
  
--- lib/database/models/order.model.ts
+++ lib/database/models/order.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, models } from 'mongoose'
+import { Document, Schema, model, models } from 'mongoose';
 
 
 export interface IOrder extends Document {
