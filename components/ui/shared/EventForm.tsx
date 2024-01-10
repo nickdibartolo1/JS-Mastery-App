859f0cd833445c4431f1cf2ac778a1879582d3fb
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-10 17:17:47 -0500

    finished ret of form added price and url inputs

--- components/ui/shared/EventForm.tsx
+++ components/ui/shared/EventForm.tsx
@@ -252,6 +252,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                               <Checkbox
                                 id="isFree"
                                 className="mr-2 h-5 w-5 border-2 border-primary-500"
+                                onCheckedChange={field.onChange}
+                                checked={field.value}
                               />
                             </div>
                           </FormControl>
